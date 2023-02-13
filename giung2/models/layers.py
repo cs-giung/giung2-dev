6667a1fc51e49bcd1fabd9fb20ebfcd498441d80
cs-giung <giung@kaist.ac.kr>
2023-02-13 20:04:02 +0900

    feat: add BatchEnsemble to giung2/models/layers

--- giung2/models/layers.py
+++ giung2/models/layers.py
@@ -32,3 +32,55 @@ class FilterResponseNorm(nn.Module):
         y     = gamma.reshape(1, 1, 1, -1) * y + beta.reshape(1, 1, 1, -1)
         z     = jnp.maximum(y, tau.reshape(1, 1, 1, -1))
         return z
+
+
+class DenseBatchEnsemble(nn.Dense):
+    use_bias: bool = False
+    ensemble_size: int = 1
+    use_ensemble_bias: bool = True
+    r_base: float = 1.0
+    s_base: float = 1.0
+    r_init: Callable[[PRNGKey, Shape, Dtype], Array] = jax.nn.initializers.zeros
+    s_init: Callable[[PRNGKey, Shape, Dtype], Array] = jax.nn.initializers.zeros
+
+    @nn.compact
+    def __call__(self, inputs: Array) -> Array:
+        r = self.param('batch_ensemble_r', self.r_init, (self.ensemble_size, inputs.shape[-1]), self.param_dtype)
+        s = self.param('batch_ensemble_s', self.s_init, (self.ensemble_size,    self.features), self.param_dtype)
+        b = self.param('ensemble_bias', self.bias_init, (self.features,), self.param_dtype) if self.use_ensemble_bias else None
+
+        x = jnp.reshape(inputs, (self.ensemble_size, -1) + inputs.shape[1:])
+        x, r = nn.dtypes.promote_dtype(x, r, dtype=self.dtype)
+        x = jnp.multiply(x, jnp.reshape(r + self.r_base, (self.ensemble_size,) + (1,) * (x.ndim - 2) + (-1,)))
+        y = super().__call__(x)
+        y, s = nn.dtypes.promote_dtype(y, s, dtype=self.dtype)
+        y = jnp.multiply(y, jnp.reshape(s + self.s_base, (self.ensemble_size,) + (1,) * (y.ndim - 2) + (-1,)))
+        if b is not None:
+            y += jnp.reshape(b, (1,) * (y.ndim - 1) + (-1,))
+        return y.reshape((-1,) + y.shape[2:])
+
+
+class ConvBatchEnsemble(nn.Conv):
+    use_bias: bool = False
+    ensemble_size: int = 1
+    use_ensemble_bias: bool = True
+    r_base: float = 1.0
+    s_base: float = 1.0
+    r_init: Callable[[PRNGKey, Shape, Dtype], Array] = jax.nn.initializers.zeros
+    s_init: Callable[[PRNGKey, Shape, Dtype], Array] = jax.nn.initializers.zeros
+
+    @nn.compact
+    def __call__(self, inputs: Array) -> Array:
+        r = self.param('batch_ensemble_r', self.r_init, (self.ensemble_size, inputs.shape[-1]), self.param_dtype)
+        s = self.param('batch_ensemble_s', self.s_init, (self.ensemble_size,    self.features), self.param_dtype)
+        b = self.param('ensemble_bias', self.bias_init, (self.features,), self.param_dtype) if self.use_ensemble_bias else None
+
+        x = jnp.reshape(inputs, (self.ensemble_size, -1) + inputs.shape[1:])
+        x, r = nn.dtypes.promote_dtype(x, r, dtype=self.dtype)
+        x = jnp.multiply(x, jnp.reshape(r + self.r_base, (self.ensemble_size,) + (1,) * (x.ndim - 2) + (-1,)))
+        y = super().__call__(x)
+        y, s = nn.dtypes.promote_dtype(y, s, dtype=self.dtype)
+        y = jnp.multiply(y, jnp.reshape(s + self.s_base, (self.ensemble_size,) + (1,) * (y.ndim - 2) + (-1,)))
+        if b is not None:
+            y += jnp.reshape(b, (1,) * (y.ndim - 1) + (-1,))
+        return y.reshape((-1,) + y.shape[2:])
