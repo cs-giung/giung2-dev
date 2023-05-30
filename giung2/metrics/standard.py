a9ce45a42a07dbb80c93aec4e87dc661c6783824
cs-giung <giung@kaist.ac.kr>
2023-05-31 01:16:46 +0900

    feat: update giung2/metrics

--- giung2/metrics/standard.py
+++ giung2/metrics/standard.py
@@ -11,21 +11,16 @@ __all__ = [
 ]
 
 
-def onehot(labels, num_classes, on_value=1.0, off_value=0.0):
-    x = (labels[..., None] == jnp.arange(num_classes).reshape((1,) * labels.ndim + (-1,)))
-    x = jax.lax.select(x, jnp.full(x.shape, on_value), jnp.full(x.shape, off_value))
-    return x.astype(jnp.float32)
-
-
 def temperature_scaling(confidences, temperature, log_input=True, eps=1e-8):
     """
     Args:
-        confidences (Array): An array with shape [N, K,].
-        temperature (float): Specifies temperature value for smoothing.
-        log_input (bool): Specifies whether confidences are already given as log values.
-        eps (float): Small value to avoid evaluation of log(0) when log_input is False.
+        confidences: An array with shape [N, K,].
+        temperature: Specifies temperature value for smoothing.
+        log_input: Specifies whether confidences are given as log values.
+        eps: Small value to avoid evaluation of log(0) when log_input is False.
     Returns:
-        An array of temperature-scaled confidences or log-confidences with shape [N, K,].
+        An array of temperature-scaled confidences or log-confidences with
+        shape [N, K,].
     """
     if log_input:
         # it returns temperature-scaled log_confidences when log_input is True.
@@ -38,13 +33,13 @@ def temperature_scaling(confidences, temperature, log_input=True, eps=1e-8):
 def get_optimal_temperature(confidences, true_labels, log_input=True, eps=1e-8):
     """
     Args:
-        confidences (Array): An array with shape [N, K,].
-        true_labels (Array): An array with shape [N,].
-        log_input (bool): Specifies whether confidences are already given as log values.
-        eps (float): Small value to avoid evaluation of log(0) when log_input is False.
+        confidences: An array with shape [N, K,].
+        temperature: Specifies temperature value for smoothing.
+        log_input: Specifies whether confidences are given as log values.
+        eps: Small value to avoid evaluation of log(0) when log_input is False.
     Returns:
-        An array of temprature with shape [1,] which minimizes negative log-likelihood for given
-        temperature-scaled confidences and true_labels.
+        An array of temprature with shape [1,] which minimizes negative
+        log-likelihood for given temperature-scaled confidences and true_labels.
     """
     log_confidences = confidences if log_input else jnp.log(confidences + eps)
     def obj(t):
@@ -53,21 +48,23 @@ def get_optimal_temperature(confidences, true_labels, log_input=True, eps=1e-8):
                 log_confidences, t, log_input=True
             ), true_labels, log_input=True
         )
-    optimal_temperature = minimize(obj, jnp.asarray([1.0,]), method='BFGS', tol=1e-3).x[0]
+    optimal_temperature = minimize(
+        obj, jnp.asarray([1.0,]), method='BFGS', tol=1e-3).x[0]
     return optimal_temperature
 
 
-def evaluate_acc(confidences, true_labels, log_input=True, eps=1e-8, reduction="mean"):
+def evaluate_acc(confidences, true_labels,
+                 log_input=True, eps=1e-8, reduction="mean"):
     """
     Args:
-        confidences (Array): An array with shape [N, K,].
-        true_labels (Array): An array with shape [N,].
-        log_input (bool, unused): Specifies whether confidences are already given as log values.
-        eps (float, unused): Small value to avoid evaluation of log(0) when log_input is False.
-        reduction (str): Specifies the reduction to apply to the output.
+        confidences: An array with shape [N, K,].
+        temperature: Specifies temperature value for smoothing.
+        log_input: Specifies whether confidences are given as log values.
+        eps: Small value to avoid evaluation of log(0) when log_input is False.
+        reduction: Specifies the reduction to apply to the output.
     Returns:
-        An array of accuracy with shape [1,] when reduction in ["mean", "sum",], or raw accuracy
-        values with shape [N,] when reduction in ["none",].
+        An array of accuracy with shape [1,] when reduction in ["mean", "sum",],
+        or raw accuracy values with shape [N,] when reduction in ["none",].
     """
     pred_labels = jnp.argmax(confidences, axis=1)
     raw_results = jnp.equal(pred_labels, true_labels)
@@ -81,21 +78,25 @@ def evaluate_acc(confidences, true_labels, log_input=True, eps=1e-8, reduction="
         raise NotImplementedError(f'Unknown reduction=\"{reduction}\"')
 
 
-def evaluate_nll(confidences, true_labels, log_input=True, eps=1e-8, reduction="mean"):
+def evaluate_nll(confidences, true_labels,
+                 log_input=True, eps=1e-8, reduction="mean"):
     """
     Args:
-        confidences (Array): An array with shape [N, K,].
-        true_labels (Array): An array with shape [N,].
-        log_input (bool): Specifies whether confidences are already given as log values.
-        eps (float): Small value to avoid evaluation of log(0) when log_input is False.
-        reduction (str): Specifies the reduction to apply to the output.
+        confidences: An array with shape [N, K,].
+        temperature: Specifies temperature value for smoothing.
+        log_input: Specifies whether confidences are given as log values.
+        eps: Small value to avoid evaluation of log(0) when log_input is False.
+        reduction: Specifies the reduction to apply to the output.
     Returns:
-        An array of negative log-likelihood with shape [1,] when reduction in ["mean", "sum",], or
-        raw negative log-likelihood values with shape [N,] when reduction in ["none",].
+        An array of negative log-likelihood with shape [1,] when reduction in
+        ["mean", "sum",], or raw negative log-likelihood values with shape [N,]
+        when reduction in ["none",].
     """
     log_confidences = confidences if log_input else jnp.log(confidences + eps)
-    true_target = onehot(true_labels, num_classes=log_confidences.shape[1])
-    raw_results = -jnp.sum(jnp.where(true_target, true_target * log_confidences, 0.0), axis=-1)
+    true_target = jax.nn.one_hot(
+        true_labels, num_classes=log_confidences.shape[1])
+    raw_results = -jnp.sum(
+        jnp.where(true_target, true_target * log_confidences, 0.0), axis=-1)
     if reduction == "none":
         return raw_results
     elif reduction == "mean":
