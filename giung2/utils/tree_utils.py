ae24df3d2943692e726538ed645415aaf15baaab
cs-giung <giung@kaist.ac.kr>
2023-07-04 19:05:55 +0900

    feat: add giung2.utils.tree_utils.rand_like_tree

--- giung2/utils/tree_utils.py
+++ giung2/utils/tree_utils.py
@@ -2,6 +2,15 @@ import jax
 import jax.numpy as jnp
 
 
+def rand_like_tree(rng_key, target, minval=0.0, maxval=1.0):
+    treedef = jax.tree_util.tree_structure(target)
+    keys = jax.tree_util.tree_unflatten(
+        treedef, jax.random.split(rng_key, treedef.num_leaves))
+    return jax.tree_util.tree_map(
+        lambda e, k: jax.random.uniform(
+            k, e.shape, e.dtype, minval=minval, maxval=maxval), target, keys)
+
+
 def randn_like_tree(rng_key, target, mean=0.0, std=1.0):
     treedef = jax.tree_util.tree_structure(target)
     keys = jax.tree_util.tree_unflatten(
