34cde1e82b5604c446e6c6ea93d880d6d835a075
cs-giung <giung@kaist.ac.kr>
2023-10-22 13:04:24 +0000

    feat: update projects/vision-transformers/scripts

---
+++ projects/vision-transformers/scripts/defaults.py
@@ -0,0 +1,35 @@
+import argparse
+
+
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+
+def default_argument_parser():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '--data_root', default='/mnt/local/datasets/manual/', type=str,
+        help='path to the directory (default: /mnt/local/datasets/manual/)')
+    parser.add_argument(
+        '--data_name', default='imagenet2012', type=str,
+        choices=['imagenet2012',])
+    
+    parser.add_argument(
+        '--batch_size', default=4096, type=int,
+        help='the number of examples for each mini-batch (default: 4096)')
+    parser.add_argument(
+        '--num_workers', default=32, type=int,
+        help='how many subprocesses to use for data loading (default: 32)')
+    parser.add_argument(
+        '--prefetch_factor', default=2, type=int,
+        help='number of batches loaded in advance by each worker (default: 2)')
+
+    return parser
